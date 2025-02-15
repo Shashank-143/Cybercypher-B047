f32530f5fde32bbadd0798707c11fd411948a549
jashan7305 <jashan070305@gmail.com>
2025-02-16 01:37:38 +0530

    static

--- app.py
+++ app.py
@@ -1,1 +1,24 @@
 from flask import Flask, jsonify, request, render_template, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route("/", methods = ["GET", "POST"])
+def home():
+    return render_template("home.html")
+
+@app.route("/trends", methods = ["GET", "POST"])
+def trends():
+    return render_template("trends.html")
+
+@app.route("/form", methods = ["GET", "POST"])
+def form():
+    return render_template("googleForm.html")
+
+@app.route("/mentor", methods = ["GET", "POST"])
+def mentor():
+    return render_template("")
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
