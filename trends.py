bca2e4590c20161cc8cf208cd2be6892697ed0aa
uselesshumanbeing <poojapashine01@gmail.com>
2025-02-16 01:18:28 +0530

    tr

---
+++ trends.py
@@ -0,0 +1,72 @@
+from flask import Flask, request, jsonify
+import os
+import ast
+from huggingface_hub import InferenceClient
+from dotenv import load_dotenv
+import requests
+
+app = Flask(__name__)
+
+apikey = os.getenv("apikey")
+repo_id = "mistralai/Mistral-7B-Instruct-v0.3"
+
+client = InferenceClient(api_key=apikey)
+def get_searches(query):
+    load_dotenv()
+    API_KEY = os.getenv("API_KEY")
+    query = query
+
+    params_related = {"q": query, "api_key": API_KEY, "engine": "google","nums":200}
+    related_res = requests.get("https://serpapi.com/search", params=params_related)
+
+    params_auto = {"q": query, "api_key": API_KEY, "engine": "google_autocomplete"}
+    auto_res = requests.get("https://serpapi.com/search", params=params_auto)
+
+    related_searches = related_res.json().get("related_searches", []) if related_res.status_code == 200 else []
+    autocomplete_suggestions = auto_res.json().get("suggestions", []) if auto_res.status_code == 200 else []
+
+    context=[]
+    for i, item in enumerate(related_searches, 1):
+        if ((list(item.keys()))[1])=='query':
+            context.append(item['query'])
+
+    for i, item in enumerate(autocomplete_suggestions, len(related_searches) + 1):
+        context.append(item['value'])
+    return check_searches(context, query)
+
+
+def check_searches(context, query):
+    query_template = f"""
+    Given the context: {context}, filter it based on the query: {query}. 
+    If the context is unrelated to {query}, do not return anything. 
+    Otherwise, return the relevant results as a list separated by commas. 
+    Do not include movies or songs or books. Make sure the output is a unique list.
+    """
+
+    messages = [
+        {"role": "system", "content": query_template},
+        {"role": "user", "content": f"Context: {context}\n\nQuestion: {query}"}
+    ] 
+
+    response = client.chat.completions.create(
+        model=repo_id,
+        messages=messages
+    )
+
+    return string_to_list(response['choices'][0]['message']['content'])
+
+def string_to_list(string_input):
+    try:
+        return ast.literal_eval(string_input)
+    except (ValueError, SyntaxError):
+        print("Invalid input format")
+        return []
+
+@app.route("/search")
+def search():
+    query = request.args.get("query", "")
+    context = get_searches(query)
+    return context
+
+if __name__ == "__main__":
+    app.run(debug=True)
